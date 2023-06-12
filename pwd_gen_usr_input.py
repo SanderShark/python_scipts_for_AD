b1578ea24dffefc267c52a05032c343a5406312c
Sam <134541524+ssanderson93@users.noreply.github.com>
2023-06-11 21:37:20 -0400

    Update pwd_gen_usr_input.py
    Updated with better complexity checking

--- pwd_gen_usr_input.py
+++ pwd_gen_usr_input.py
@@ -7,29 +7,19 @@ digit_chars = string.digits
 special_chars = string.punctuation
 alphabet = letter_chars + digit_chars + special_chars
 
-
-
-# generate password meeting length requirement
 def pwd_gen():
-    
-    #input is a local variable(needs validation)
+    # input is a local variable (needs validation)
     pwd_length = int(input())
-    
-    
-    #local vars
-    pwd = ''
-    
 
-        
-    #character iteration
-    for i in range(pwd_length):
-        pwd += ''.join(secrets.choice(alphabet))
-        
-        
-        #complexity requirements check loop, can be changed to add upper and lower case letter requirements
-        if (any(char in special_chars for char in pwd) and 
-            sum(char in digit_chars for char in pwd)>=2):
-            break
+    # local vars
+    pwd = ''
+    complexity_met = False
     
-            
+    while not complexity_met:
+        # character iteration
+        pwd = ''.join(secrets.choice(alphabet) for _ in range(pwd_length))
+
+        # complexity requirements check
+        if any(char in special_chars for char in pwd) and sum(char in digit_chars for char in pwd) >= 2:
+            complexity_met = True
     print(pwd)
